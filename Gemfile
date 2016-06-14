379c3b6923ac06ef48389dc27f37c995d794959a
gidpoiioh <adacer91092@mail.ru>
2016-06-15 02:19:42 +0300

    Devise базовая настройка https://habrahabr.ru/post/208056/ http://localhost:3000/ ответ Home#index

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'thin'
 
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '>= 5.0.0.rc1', '< 5.1'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'home/index'
+
+  devise_for :users
+
+  root 'home#index'
+  
+  resources :users
+
+  get 'persons/profile', as: 'user_root'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
